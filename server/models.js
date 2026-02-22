b9da29941015d408ec54e39a404802833e2d91b9
nravish1 <nravish1.unc.edu>
2026-02-21 20:39:06 -0500

    pushing schema

--- server/models.js
+++ server/models.js
@@ -1,14 +1,25 @@
 import mongoose from 'mongoose'
+import { type } from 'os'
 
 const userSchema = new mongoose.Schema({
+  name: String,
   email: String,
   password: String,
   plaidAccessToken: String,
+  interests: [String],
+  goal: {
+    label: String,
+    targetAmount: Number,
+    savedAmount: Number,
+    deadline: Date
+    
+  },
   budget: {
     total: Number,
     spent: { type: Number, default: 0 },
     categories: Object
   }
+
 })
 
 const purchaseSchema = new mongoose.Schema({
